a5282419cbcfb31ececc001aded182e95f01091b
khanh <khanhnc46@gmail.com>
2019-10-22 09:13:24 +1100

    Updates

---
+++ precache-manifest.016bf93d9526962fabde553bfd24f726.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e040cbf7d88a752b2e0060f33147e7b3",
+    "url": "/khanhweb/index.html"
+  },
+  {
+    "revision": "e992359c5e4dbfd94ce6",
+    "url": "/khanhweb/static/css/main.e589183f.chunk.css"
+  },
+  {
+    "revision": "e0072ea4d34be7096ad6",
+    "url": "/khanhweb/static/js/2.bf22980a.chunk.js"
+  },
+  {
+    "revision": "e992359c5e4dbfd94ce6",
+    "url": "/khanhweb/static/js/main.9ce2ba08.chunk.js"
+  },
+  {
+    "revision": "ce10c5b0adcf2427cdaa",
+    "url": "/khanhweb/static/js/runtime-main.b15bda21.js"
+  },
+  {
+    "revision": "34fb9f33bcf9ef509893dc21f3aa1723",
+    "url": "/khanhweb/static/media/linkin.34fb9f33.jpg"
+  },
+  {
+    "revision": "a21654dd37abc8e9d06087ad9bed27f5",
+    "url": "/khanhweb/static/media/resume.a21654dd.pdf"
+  }
+]);
