a3753d00576f429e41fcd667f3e4cfc88e179652
khanh <khanhnc46@gmail.com>
2019-10-22 12:37:27 +1100

    Updates

---
+++ precache-manifest.b102b4efd91f5661c1c8be2b442bb4bc.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5eb84a9fb238446a54aabd567636aec",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "af9717f5bca2eca38a6c",
+    "url": "/portfolio/static/css/main.9da4de55.chunk.css"
+  },
+  {
+    "revision": "2578e0bf7838bfc9f424",
+    "url": "/portfolio/static/js/2.d715701d.chunk.js"
+  },
+  {
+    "revision": "af9717f5bca2eca38a6c",
+    "url": "/portfolio/static/js/main.0a491d55.chunk.js"
+  },
+  {
+    "revision": "1d01becbf82180316dd0",
+    "url": "/portfolio/static/js/runtime-main.b6bab58d.js"
+  },
+  {
+    "revision": "f63021ef85c2b8fe600c57e79e9b2094",
+    "url": "/portfolio/static/media/bcapp1.f63021ef.png"
+  },
+  {
+    "revision": "1ec07d5d4d2c0e44abf6292256e9c5ab",
+    "url": "/portfolio/static/media/faeth1.1ec07d5d.png"
+  },
+  {
+    "revision": "56ea954799049d9b572287765790d36a",
+    "url": "/portfolio/static/media/faeth2.56ea9547.png"
+  },
+  {
+    "revision": "51983abe07b2bdb2dd5c69f25dcd0ef4",
+    "url": "/portfolio/static/media/faeth3.51983abe.png"
+  },
+  {
+    "revision": "9855fbab3c03d73f9d4ac73923807087",
+    "url": "/portfolio/static/media/faethlogo.9855fbab.png"
+  },
+  {
+    "revision": "cd1106a166219c26d06c39332ba091b4",
+    "url": "/portfolio/static/media/festival1.cd1106a1.png"
+  },
+  {
+    "revision": "8a31a40804246a653f4c6f21f8fe6214",
+    "url": "/portfolio/static/media/festival2.8a31a408.png"
+  },
+  {
+    "revision": "217e5dcbdfb854e6513c9be1da02afa0",
+    "url": "/portfolio/static/media/festival3.217e5dcb.png"
+  },
+  {
+    "revision": "d5d80dc44d920970619afa4160a644d0",
+    "url": "/portfolio/static/media/festival4.d5d80dc4.png"
+  },
+  {
+    "revision": "34fb9f33bcf9ef509893dc21f3aa1723",
+    "url": "/portfolio/static/media/linkin.34fb9f33.jpg"
+  },
+  {
+    "revision": "b279efe5190995ab0bf7f77caf2af128",
+    "url": "/portfolio/static/media/logo.b279efe5.png"
+  },
+  {
+    "revision": "d5975dad48cc9e498cd5c5c9d5b50576",
+    "url": "/portfolio/static/media/mdes1.d5975dad.png"
+  },
+  {
+    "revision": "cc6ba3059bb180f6452e5d9b8c67fa39",
+    "url": "/portfolio/static/media/mp1.cc6ba305.png"
+  },
+  {
+    "revision": "ac7821673e1a6a3cc5bc39097644b90e",
+    "url": "/portfolio/static/media/mp2.ac782167.png"
+  },
+  {
+    "revision": "ea323ec819f6ce2a15c2684320357e4d",
+    "url": "/portfolio/static/media/mp3.ea323ec8.png"
+  },
+  {
+    "revision": "3037e65693aa9d9236d9058e693215b7",
+    "url": "/portfolio/static/media/mp4.3037e656.png"
+  },
+  {
+    "revision": "c0b0195b5a7fd68d7fc532e5d27518c3",
+    "url": "/portfolio/static/media/mpf1.c0b0195b.png"
+  },
+  {
+    "revision": "a4be4e8abfd544a573e0ca0d1c5e2ab8",
+    "url": "/portfolio/static/media/mpflogo.a4be4e8a.png"
+  },
+  {
+    "revision": "ef7f057676565b5dca0daec6e1478ca0",
+    "url": "/portfolio/static/media/page1.ef7f0576.png"
+  },
+  {
+    "revision": "979483d3e68cab3c5acd0b473f258adf",
+    "url": "/portfolio/static/media/page2.979483d3.png"
+  },
+  {
+    "revision": "b3582300003d1f426b75c56f8635c057",
+    "url": "/portfolio/static/media/page3.b3582300.png"
+  },
+  {
+    "revision": "6835b2263a1ed15bcfbe257b9c871f49",
+    "url": "/portfolio/static/media/personal.6835b226.png"
+  },
+  {
+    "revision": "a21654dd37abc8e9d06087ad9bed27f5",
+    "url": "/portfolio/static/media/resume.a21654dd.pdf"
+  },
+  {
+    "revision": "132625a00b2d21f7eaeee8c17f9a2310",
+    "url": "/portfolio/static/media/stav1.132625a0.png"
+  },
+  {
+    "revision": "cf91675ae6093f58db2cae47e2655d75",
+    "url": "/portfolio/static/media/stav2.cf91675a.png"
+  },
+  {
+    "revision": "a9d508b495bd307b16411cfb0f8e7733",
+    "url": "/portfolio/static/media/stav3.a9d508b4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/khanhweb/precache-manifest.eb1a4c3058675a3c49ad5bf74ae508fe.js"
+  "/portfolio/precache-manifest.b102b4efd91f5661c1c8be2b442bb4bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/khanhweb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
